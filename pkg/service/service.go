7506a653fdcb3fd30462aad277a579cf92d518ac
MrSR <alexeeval.1996@gmail.com>
2022-11-22 19:34:28 +0300

    add authorization

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,8 +1,12 @@
 package service
 
-import "github.com/azonnix/todo-app/pkg/repository"
+import (
+	"github.com/azonnix/todo-app"
+	"github.com/azonnix/todo-app/pkg/repository"
+)
 
-type Authorization struct {
+type Authorization interface {
+	CreateUser(user todo.User) (int, error)
 }
 
 type TodoList struct {
@@ -18,5 +22,7 @@ type Service struct {
 }
 
 func NewService(repos *repository.Repository) *Service {
-	return &Service{}
+	return &Service{
+		Authorization: NewAuthService(repos.Authorization),
+	}
 }
